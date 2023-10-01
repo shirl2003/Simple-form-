191fc3597bfeff3ad34d46f559c4d47ad9c52c96
shirl2003 <144830886+shirl2003@users.noreply.github.com>
2023-10-01 12:33:40 +0530

    Add files via upload

---
+++ link.php
@@ -0,0 +1,9 @@
+
+<script src ="https://cdn.usebootstrap.com/bootstrap/latest/js/bootstrap.min.js"></script>
+<link rel="stylesheet"  href="https://cdn.usebootstrap.com/bootstrap/latest/css/bootstrap.min.css">
+<meta charset= "utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script src="https://cdn.jsdelivr.net/npm/googleapis@122.0.0/build/src/index.min.js"></script>
+<script>
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/fonts/fontawesome-webfont.ttf"
+</script>
